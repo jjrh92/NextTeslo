575005805a8f68121f7ee65f1d788544a78b7bca
Julio Reyes <jreyes@aaxisdigital.com>
2024-07-23 09:56:14 -0400

    updating next version & .env template

---
+++ auth.config.ts
@@ -0,0 +1,12 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+    pages: {
+        signIn: "/auth/login",
+        newUser: "auth/new-account",
+    },
+    providers: [
+
+        
+    ]
+}
